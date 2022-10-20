15640c0cfb9af0c602a529df9589cab8f79c979d
DESKTOP-E9NGIOJ\hapro <hanthe140188@fpt.edu.vn>
2022-10-20 14:02:40 +0700

    Update items.constants.js

--- manager/src/constants/items.constants.js
+++ manager/src/constants/items.constants.js
@@ -10,49 +10,49 @@ export const siderBarItems = [
           key: "product",
           icon: <TeamOutlined style={{ fontSize: "1.6rem" }} />,
           label: `Product`,
-          role: "admin employee",
+          role: ["admin", "employee"],
      },
      {
           key: "brand",
           icon: <TeamOutlined style={{ fontSize: "1.6rem" }} />,
           label: `Brand`,
-          role: "admin employee",
+          role: ["admin", "employee"],
      },
      {
           key: "accounts",
           icon: <TeamOutlined style={{ fontSize: "1.6rem" }} />,
           label: `Account`,
-          role: "admin",
+          role: ["admin"],
      },
      {
           key: "employee",
           icon: <TeamOutlined style={{ fontSize: "1.6rem" }} />,
           label: `Employee`,
-          role: "admin",
+          role: ["admin"],
      },
      {
           key: "supplier",
           icon: <FileDoneOutlined style={{ fontSize: "1.6rem" }} />,
           label: `Supplier`,
-          role: "admin employee",
+          role: ["admin", "employee"],
      },
      {
           key: "customer",
           icon: <ReconciliationOutlined style={{ fontSize: "1.6rem" }} />,
           label: `Customer`,
-          role: "admin employee",
+          role: ["admin", "employee"],
      },
-		//  
+     //
      {
           key: "profile",
           icon: <ReconciliationOutlined style={{ fontSize: "1.6rem" }} />,
           label: `Profile`,
-          role: "employee",
+          role: ["employee"],
      },
      {
           key: "settings",
           icon: <ReconciliationOutlined style={{ fontSize: "1.6rem" }} />,
           label: `Settings`,
-          role: "admin employee",
+          role: ["admin", "employee"],
      },
 ];
