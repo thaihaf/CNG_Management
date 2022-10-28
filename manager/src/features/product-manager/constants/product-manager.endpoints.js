4de899ff723b557a5b4b01427087ce6c79d8e029
DESKTOP-E9NGIOJ\hapro <hanthe140188@fpt.edu.vn>
2022-10-28 13:06:03 +0700

    fix slow compile

--- manager/src/features/product-manager/constants/product-manager.endpoints.js
+++ manager/src/features/product-manager/constants/product-manager.endpoints.js
@@ -1,4 +1,6 @@
 export const ProductEndPoints = {
-     PRODUCT_MANAGER: "/product",
-     PRODUCT_DETAILS: "/product/:productId",
+  PRODUCT_MANAGER: "/product",
+  PRODUCT_DETAILS: "/product/:productId",
+  PRODUCT_IMPORT_MANAGER: "/import-product",
+  PRODUCT_IMPORT_DETAILS: "/import-product/:importId",
 };
--- manager/src/features/product-manager/routes/product-manager.routes.js
+++ manager/src/features/product-manager/routes/product-manager.routes.js
@@ -3,41 +3,75 @@ import React from "react";
 import { ProductManagerPaths } from "../constants/product-manager.paths";
 
 const ProductListScreen = React.lazy(() =>
-     import("../screens/ProductList/ProductList")
+  import("../screens/ProductList/ProductList")
 );
 const CreateProductScreen = React.lazy(() =>
-     import("../screens/CreateProduct/CreateProduct")
+  import("../screens/CreateProduct/CreateProduct")
 );
 const ProductDetailsScreen = React.lazy(() =>
-     import("../screens/ProductDetails/ProductDetails")
+  import("../screens/ProductDetails/ProductDetails")
+);
+const DetailsProductImportScreen = React.lazy(() =>
+  import("../screens/ImportProduct/DetailsProductImport/DetailsProductImport")
+);
+const ListProductImportScreen = React.lazy(() =>
+  import("../screens/ImportProduct/ListProductImport/ListProductImport")
+);
+const CreateProductImportScreen = React.lazy(() =>
+  import("../screens/ImportProduct/CreateProductImport/CreateProductImport")
 );
 
 const PRODUCT_LIST_SCREEN = {
-     id: "product-list",
-     path: ProductManagerPaths.PRODUCT_MANAGER,
-     component: ProductListScreen,
-     isPrivateRoute: true,
-     pageTitle: "Product Manager",
+  id: "product-list",
+  path: ProductManagerPaths.PRODUCT_MANAGER,
+  component: ProductListScreen,
+  isPrivateRoute: true,
+  pageTitle: "Product Manager",
 };
 const CREATE_PRODUCT_SCREEN = {
-     id: "create-product",
-     path: ProductManagerPaths.CREATE_PRODUCT,
-     component: CreateProductScreen,
-     isPrivateRoute: true,
-     pageTitle: "Create Product",
+  id: "create-product",
+  path: ProductManagerPaths.CREATE_PRODUCT,
+  component: CreateProductScreen,
+  isPrivateRoute: true,
+  pageTitle: "Create Product",
 };
 const PRODUCT_DETAILS_SCREEN = {
-     id: "product-details",
-     path: ProductManagerPaths.PRODUCT_DETAILS,
-     component: ProductDetailsScreen,
-     isPrivateRoute: true,
-     pageTitle: "Product Details",
+  id: "product-details",
+  path: ProductManagerPaths.PRODUCT_DETAILS,
+  component: ProductDetailsScreen,
+  isPrivateRoute: true,
+  pageTitle: "Product Details",
+};
+
+const DETAILS_PRODUCT_IMPORT_SCREEN = {
+  id: "details-product-import",
+  path: ProductManagerPaths.DETAILS_PRODUCT_IMPORT,
+  component: DetailsProductImportScreen,
+  isPrivateRoute: true,
+  pageTitle: "Details Product Import",
+};
+const LIST_PRODUCT_IMPORT_SCREEN = {
+  id: "list-product-import",
+  path: ProductManagerPaths.LIST_PRODUCT_IMPORT,
+  component: ListProductImportScreen,
+  isPrivateRoute: true,
+  pageTitle: "List Product Import",
+};
+const CREATE_PRODUCT_IMPORT_SCREEN = {
+  id: "create-product-import",
+  path: ProductManagerPaths.CREATE_PRODUCT_IMPORT,
+  component: CreateProductImportScreen,
+  isPrivateRoute: true,
+  pageTitle: "Create Product Import",
 };
 
 const PRODUCT_MANAGER_ROUTES = [
-     CREATE_PRODUCT_SCREEN,
-     PRODUCT_DETAILS_SCREEN,
-     PRODUCT_LIST_SCREEN,
+  CREATE_PRODUCT_SCREEN,
+  PRODUCT_DETAILS_SCREEN,
+  PRODUCT_LIST_SCREEN,
+  DETAILS_PRODUCT_IMPORT_SCREEN,
+  LIST_PRODUCT_IMPORT_SCREEN,
+  CREATE_PRODUCT_IMPORT_SCREEN,
 ];
 
 export default PRODUCT_MANAGER_ROUTES;
