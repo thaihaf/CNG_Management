12a947466f8d509dfdeba2227ccac1d6b0cc7463
DESKTOP-E9NGIOJ\hapro <hanthe140188@fpt.edu.vn>
2022-11-28 18:09:01 +0700

    delete warehouse

--- manager/src/features/export-product/constants/export-product.endpoints.js
+++ manager/src/features/export-product/constants/export-product.endpoints.js
@@ -5,5 +5,5 @@ export const ExportProductEndPoints = {
   PRODUCT_EXPORT_DETAIL_WAREHOUSE: "/export-product/detail-warehouse/:id",
   DELETE_PRODUCT_EXPORT: "/export-product/delete-all/:id",
   DELETE_DETAILS_PRODUCT_EXPORT: "/export-product/delete-one/:id",
-  DELETE_WAREHOUSE_PRODUCT_EXPORT: "/export-product/delete-warehouse",
+  DELETE_WAREHOUSE_PRODUCT_EXPORT: "/export-product/delete-warehouse/:id",
 };
